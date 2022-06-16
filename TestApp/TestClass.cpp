c3805cdce4552b7135f86c2e46af222f09b1fa66
tetsuro abe <tetsuro.abe@pioneerdj.com>
2022-06-16 10:13:49 +0900

    add files

---
+++ TestApp/TestClass.cpp
@@ -0,0 +1,23 @@
+//
+//  TestClass.cpp
+//  TestApp
+//
+//  Created by pdj on 2022/06/16.
+//
+
+#include "TestClass.hpp"
+#include <iostream>
+
+TestClass::TestClass() {
+    std::cout << __FUNCTION__ << std::endl;
+}
+
+TestClass::~TestClass() {
+    std::cout << __FUNCTION__ << std::endl;
+}
+
+int TestClass::func(const int a, const int b) {
+    std::cout << __FUNCTION__ << std::endl;
+
+    return a + b;
+}
---
+++ TestApp/TestClass.hpp
@@ -0,0 +1,41 @@
+/**
+* @file TestClass.h
+* @brief 簡単な説明
+* @author 作成者
+* @date 作成日
+* @details 詳細な説明
+*/
+
+#ifndef TestClass_hpp
+#define TestClass_hpp
+
+#include <stdio.h>
+
+/**
+* @brief テストクラス 何もしない
+* @details 詳細はここに書く
+*/
+class TestClass {
+public:
+    /**
+    * @brief コンストラクタ
+    */
+    TestClass();
+
+    /** デストラクタ */
+    ~TestClass();
+    
+    /**
+    * @brief func
+    * @param[in] a 引数の説明
+    * @param[in] b 引数の説明
+    * @return int aとbの和を返す
+    * @details 詳細な説明
+    */
+    int func(const int a, const int b);
+private:
+    //! 変数 hoge あれこれ用途
+    int hoge = 1;
+};
+
+#endif /* TestClass_hpp */
